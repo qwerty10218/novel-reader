2fd8e6ed6b967226b1921b51056dd2e328c8d2be
Gary <qwerty10218@users.noreply.github.com>
2026-09-09 13:54:15 +0800

    fix: 更新啟動批次檔編碼支援 UTF-8

--- 啟動測試網頁.bat
+++ 啟動測試網頁.bat
@@ -1,6 +1,7 @@
-@echo off
-echo ���b�Ұʴ��զ��A��...
-echo �Ф��n�����o�Ӷ¦�����A�s�����Y�N�۰ʥ��}�C
+﻿@echo off
+chcp 65001 >nul
+echo 正在啟動彼岸仍是人間小說閱讀器測試伺服器...
+echo 請不要關閉這個黑視窗，瀏覽器即將自動開啟。
 start http://localhost:8000
 python -m http.server 8000
 pause
